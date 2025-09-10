498a137a6c55118988f3e83c74add85c9bd900aa
Antonio Calderon <antoniocalderon96@hotmail.com>
2025-09-10 08:53:23 -0500

    First init

---
+++ jellyfin/config/data/data/ScheduledTasks/7d8088c1-0902-f1bf-4072-ded42437bcfb.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2025-09-10T13:49:16.9522372Z","EndTimeUtc":"2025-09-10T13:49:16.9555512Z","Status":"Completed","Name":"Clean Transcode Directory","Key":"DeleteTranscodeFiles","Id":"7d8088c10902f1bf4072ded42437bcfb"}
